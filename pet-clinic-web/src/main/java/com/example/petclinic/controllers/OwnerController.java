1045215b12fa580285ad6a676e473c5830f6e20a
unknown <pdpawelczak@gmail.com>
2021-02-10 18:16:22 +0100

    owner,vet controller and page

---
+++ pet-clinic-web/src/main/java/com/example/petclinic/controllers/OwnerController.java
@@ -0,0 +1,15 @@
+package com.example.petclinic.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/owners")
+public class OwnerController {
+
+    @GetMapping({"", "/index"})
+    public String listOwners(){
+        return "owners/index";
+    }
+}
