95d8846de7f0493d502d886b94bcf6f5dea375f9
unknown <pdpawelczak@gmail.com>
2021-02-11 15:32:32 +0100

    Auto id map generation

--- pet-clinic-web/src/main/java/com/example/petclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/com/example/petclinic/bootstrap/DataLoader.java
@@ -21,13 +21,11 @@ public class DataLoader implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
         Owner owner1 = new Owner();
-        owner1.setId(1L);
         owner1.setFirstName("Peter");
         owner1.setLastName("Pan");
         ownerService.save(owner1);
 
         Owner owner2 = new Owner();
-        owner2.setId(2L);
         owner2.setFirstName("Jar-jar");
         owner2.setLastName("Binks");
         ownerService.save(owner2);
@@ -35,13 +33,11 @@ public class DataLoader implements CommandLineRunner {
         System.out.println("Bootstrapping owner data");
 
         Vet vet1 = new Vet();
-        vet1.setId(1L);
         vet1.setFirstName("Sith");
         vet1.setLastName("Lord");
         vetService.save(vet1);
 
         Vet vet2 = new Vet();
-        vet2.setId(2L);
         vet2.setFirstName("Barbara");
         vet2.setLastName("Kowalska");
         vetService.save(vet2);
