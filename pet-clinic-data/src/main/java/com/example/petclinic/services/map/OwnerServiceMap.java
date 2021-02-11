be7c3c2ffc3d1c2c8a08d54efb743d596eede349
unknown <pdpawelczak@gmail.com>
2021-02-11 20:40:51 +0100

    Added pets and contant to bootstrap data loader

--- pet-clinic-data/src/main/java/com/example/petclinic/services/map/OwnerServiceMap.java
+++ pet-clinic-data/src/main/java/com/example/petclinic/services/map/OwnerServiceMap.java
@@ -2,10 +2,33 @@ package com.example.petclinic.services.map;
 
 import com.example.petclinic.model.Owner;
 import com.example.petclinic.services.OwnerService;
+import com.example.petclinic.services.PetService;
+import com.example.petclinic.services.PetTypeService;
 import org.springframework.stereotype.Service;
 
 @Service
 public class OwnerServiceMap extends  AbstractMapService<Owner> implements OwnerService {
+
+    private final PetTypeService petTypeService;
+    private final PetService petService;
+
+    public OwnerServiceMap(PetTypeService petTypeService, PetService petService) {
+        this.petTypeService = petTypeService;
+        this.petService = petService;
+    }
+
+    @Override
+    public Owner save(Owner object) {
+        object.getPets().forEach(pet -> {
+            if(pet.getId() == null){
+                petService.save(pet);
+            }
+            if(pet.getPetType().getId() == null){
+                petTypeService.save(pet.getPetType());
+            }
+        });
+        return super.save(object);
+    }
 
     @Override
     public Owner findByLastName(String lastName) {
--- pet-clinic-web/src/main/java/com/example/petclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/com/example/petclinic/bootstrap/DataLoader.java
@@ -1,34 +1,74 @@
 package com.example.petclinic.bootstrap;
 
 import com.example.petclinic.model.Owner;
+import com.example.petclinic.model.Pet;
+import com.example.petclinic.model.PetType;
 import com.example.petclinic.model.Vet;
 import com.example.petclinic.services.OwnerService;
+import com.example.petclinic.services.PetTypeService;
 import com.example.petclinic.services.VetService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
+
+import java.time.LocalDate;
 
 @Component
 public class DataLoader implements CommandLineRunner {
 
     private final OwnerService ownerService;
     private final VetService vetService;
+    private final PetTypeService petTypeService;
 
-    public DataLoader(OwnerService ownerService, VetService vetService) {
+    public DataLoader(OwnerService ownerService, VetService vetService, PetTypeService petTypeService) {
         this.ownerService = ownerService;
         this.vetService = vetService;
+        this.petTypeService = petTypeService;
     }
 
     @Override
     public void run(String... args) throws Exception {
+
+        PetType dog = new PetType();
+        dog.setName("Dog");
+        petTypeService.save(dog);
+
+        PetType cat = new PetType();
+        cat.setName("Cat");
+        petTypeService.save(cat);
+
         Owner owner1 = new Owner();
         owner1.setFirstName("Peter");
         owner1.setLastName("Pan");
+        owner1.setAddress("Wojska polskiego 42");
+        owner1.setCity("Katowice");
+        owner1.setTelephone("128371782");
+
+        Pet peterPet = new Pet();
+        peterPet.setPetType(cat);
+        peterPet.setOwner(owner1);
+        peterPet.setBirthDate(LocalDate.now());
+        peterPet.setName("Sisi");
+        owner1.getPets().add(peterPet);
+
         ownerService.save(owner1);
 
         Owner owner2 = new Owner();
         owner2.setFirstName("Jar-jar");
         owner2.setLastName("Binks");
+        owner2.setAddress("Wojska polskiego 42");
+        owner2.setCity("Sosnowiec");
+        owner2.setTelephone("128371782");
+
+        Pet jarPet = new Pet();
+        jarPet.setPetType(dog);
+        jarPet.setOwner(owner2);
+        jarPet.setBirthDate(LocalDate.now());
+        jarPet.setName("Doggo");
+        owner2.getPets().add(jarPet);
+
         ownerService.save(owner2);
+
+
 
         System.out.println("Bootstrapping owner data");
 
